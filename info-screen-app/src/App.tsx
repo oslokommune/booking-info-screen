1e31c47f00e180b6d0b1f86453dbc5675a63bd2a
Ivar Fredriksen <ivar.fredriksen@origo.oslo.kommune.no>
2024-11-28 13:43:43 +0100

    Update info-screen with confetti

--- info-screen-app/src/App.tsx
+++ info-screen-app/src/App.tsx
@@ -1,47 +1,61 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
+import Confetti from 'react-confetti';
 import './App.css';
 
-const server = process.env.NODE_ENV === 'development' ? 'http://localhost:9090' : 'https://api.booking.oslo.kommune.no'
+const server =
+    process.env.NODE_ENV === 'development'
+        ? 'http://localhost:9090'
+        : 'https://api.booking.oslo.kommune.no';
 
 const useInfoScreen = () => {
     const [dailyBookings, setDailyBookings] = React.useState<{
-        dailyBookings: number,
-        "yesterdayBookings": number
+        dailyBookings: number;
+        yesterdayBookings: number;
     }>({
         dailyBookings: 0,
-        yesterdayBookings: 0
+        yesterdayBookings: 0,
     });
-
 
     useEffect(() => {
         let interval = setInterval(() => {
-            fetch(server + '/api/info-screen/bookings')
-                .then(async res => {
-                    setDailyBookings(await res.json())
-                })
+            fetch(server + '/api/info-screen/bookings').then(async (res) => {
+                setDailyBookings(await res.json());
+            });
         }, 60 * 1000);
 
         return () => clearInterval(interval);
     }, []);
 
-
     useEffect(() => {
-        fetch(server + '/api/info-screen/bookings')
-            .then(async res => {
-                setDailyBookings(await res.json())
-            })
-    }, [])
+        fetch(server + '/api/info-screen/bookings').then(async (res) => {
+            setDailyBookings(await res.json());
+        });
+    }, []);
 
     return {
-        dailyBookings
+        dailyBookings,
     };
-}
+};
 
 function App() {
-    let {dailyBookings: {dailyBookings, yesterdayBookings}} = useInfoScreen();
-    console.log(process.env)
+    const {
+        dailyBookings: { dailyBookings, yesterdayBookings },
+    } = useInfoScreen();
+    const [prevBookings, setPrevBookings] = useState<number>(0);
+    const [showConfetti, setShowConfetti] = useState(false);
+
+    useEffect(() => {
+        if (prevBookings !== 0 && dailyBookings > prevBookings) {
+            // Trigger confetti if bookings have increased
+            setShowConfetti(true);
+            setTimeout(() => setShowConfetti(false), 3000); // Show confetti for 3 seconds
+        }
+        setPrevBookings(dailyBookings); // Update previous bookings
+    }, [dailyBookings, prevBookings]);
+
     return (
         <div className="App">
+            {showConfetti && <Confetti />}
             <table>
                 <tbody>
                 <tr>
